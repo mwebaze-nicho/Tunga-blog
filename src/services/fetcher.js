37f37d15d6f2a7dc077802db6cbecbec28474f88
mwebaze-nicho <nickmwebaze@gmail.com>
2024-05-21 09:56:55 +0300

    Added base url to the get requests

--- src/services/fetcher.js
+++ src/services/fetcher.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-// export const axiosInstance = axios.create({
-//   baseURL: "/",
-// });
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+});
 
 const fetcher = async (url) => {
   // const res = axios.get(url).then((res) => res.data);
- try {
-   const res = await axios.get(url);
-   
-   return res.data
- } catch (error) {
-  
- }
+  try {
+    const res = await axiosInstance.get(url);
+
+    return res.data;
+  } catch (error) {}
 };
 
 export default fetcher;
