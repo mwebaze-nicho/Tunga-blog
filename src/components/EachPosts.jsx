4cb04c44483235635c62abe956d5309d46cd9ff1
mwebaze-nicho <nickmwebaze@gmail.com>
2024-05-30 21:03:14 +0300

    **

--- src/components/EachPosts.jsx
+++ src/components/EachPosts.jsx
@@ -38,7 +38,7 @@ function EachPost(props) {
                 </div>
                 <div className="flex items-center gap-4 text-xs mt-4">
                   <time className="text-gray-500">{data.data.date}</time>
-                  <span className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+                  <span className="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
                     {data.data.category}
                   </span>
                   <EditButton postId={props.id} owner={data.data.postCreator} />
